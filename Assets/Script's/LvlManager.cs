d08ba81f3efcbe364add44b4ab15d8ec928a5b7f
Diego Camilo Herrera Ferrer <danthe1996@gmail.com>
2020-11-18 17:39:39 -0500

    Actulizacion
    Se agrego:
    Panel de "Primera vez"
    Selector de avatar
    Stage avatar momentanio
    Se implemento:
    Interfaz de Darwin
    MiniJuego RA

---
+++ Assets/Script's/LvlManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvlManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    public void IrScene(int NumScene)
+    {
+        SceneManager.LoadScene(NumScene);
+    }
+
+}
